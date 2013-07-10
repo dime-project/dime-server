b1cb52772be44245652cd40309ccdfaec03ee669
simon <simon.thiel@iao.fraunhofer.de>
2013-07-10 14:44:01 +0200

    FIX some minor layout issues

--- dime-personalserver/dime-communications/src/main/webapp/static/dime/script/index.js
+++ dime-personalserver/dime-communications/src/main/webapp/static/dime/script/index.js
@@ -1464,7 +1464,7 @@ Dime.Settings = {
                     $('<img></img>')                    
                     .attr("src", Dime.psHelper.guessLinkURL(item.imageUrl))                    
                    )
-                .append("<b>" + item.name.substring(0,25) + "</b></br>")
+                .append("<b>" + item.name.substr(0,17) + "</b></br>")
                 .append(
                     $("<span></span>")
                     .addClass("serviceActiveMessage")
