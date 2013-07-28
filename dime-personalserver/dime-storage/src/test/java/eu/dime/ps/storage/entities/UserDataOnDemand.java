7d9e9c1e9c34be9faa3ced1e6f8e4eec439337ae
simon <simon.thiel@iao.fraunhofer.de>
2013-07-28 22:25:15 +0200

    LICENSE added header to all non-copyright java files

--- dime-personalserver/dime-storage/src/test/java/eu/dime/ps/storage/entities/UserDataOnDemand.java
+++ dime-personalserver/dime-storage/src/test/java/eu/dime/ps/storage/entities/UserDataOnDemand.java
@@ -1,3 +1,17 @@
+/*
+* Copyright 2013 by the digital.me project (http:\\www.dime-project.eu).
+*
+* Licensed under the EUPL, Version 1.1 only (the "Licence");
+* You may not use this work except in compliance with the Licence.
+* You may obtain a copy of the Licence at:
+*
+* http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the Licence for the specific language governing permissions and limitations under the Licence.
+*/
+
 package eu.dime.ps.storage.entities;
 
 import java.util.List;
